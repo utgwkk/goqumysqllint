53f3becf8c22caf53b347bb200dd09c6ab7fd6a7
utgwkk <utagawakiki@gmail.com>
2024-12-14 15:32:48 +0900

    first implementation (but tests not working)

--- goqumysqllint.go
+++ goqumysqllint.go
@@ -1,16 +1,12 @@
 package goqumysqllint
 
 import (
-	"errors"
-	"fmt"
 	"go/ast"
-	"go/types"
 
 	_ "github.com/doug-martin/goqu/v9" // for lookup types
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
-	"golang.org/x/tools/go/packages"
 )
 
 func NewAnalyzer(_ any) (*analysis.Analyzer, error) {
@@ -28,6 +24,10 @@ func NewAnalyzer(_ any) (*analysis.Analyzer, error) {
 	}, nil
 }
 
+const (
+	booleanExpressionFullTypeName = "github.com/doug-martin/goqu/v9/exp.BooleanExpression"
+)
+
 type analyzer struct{}
 
 func newAnalyzer() (*analyzer, error) {
@@ -35,27 +35,32 @@ func newAnalyzer() (*analyzer, error) {
 }
 
 func (a *analyzer) run(pass *analysis.Pass) (interface{}, error) {
-	it := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector).PreorderSeq()
-	pkgs, err := packages.Load(&packages.Config{
-		Mode: packages.LoadTypes | packages.NeedTypesInfo,
-	}, "github.com/doug-martin/goqu/v9/exp")
-	if err != nil {
-		return nil, err
-	}
-	goquPkg := pkgs[0]
-	boolExpTy := goquPkg.Types.Scope().Lookup("BooleanExpression").Type().Underlying().(*types.Interface)
-	if boolExpTy == nil {
-		return nil, errors.New("BooleanExpression not found")
-	}
-	fmt.Println(boolExpTy)
+	it := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector).PreorderSeq(
+		new(ast.CallExpr),
+	)
+
 	for node := range it {
-		node, ok := node.(ast.Expr)
+		node, ok := node.(*ast.CallExpr)
 		if !ok {
 			continue
 		}
 		ty := pass.TypesInfo.TypeOf(node)
-		fmt.Println(ty)
-		fmt.Println(types.Implements(ty, boolExpTy))
+		if ty.String() != booleanExpressionFullTypeName {
+			continue
+		}
+		if len(node.Args) != 1 {
+			continue
+		}
+		arg := node.Args[0]
+		argTy := pass.TypesInfo.TypeOf(arg)
+		if argTy.String() != "bool" {
+			continue
+		}
+
+		pass.Report(analysis.Diagnostic{
+			Pos:     node.Pos(),
+			Message: "compare boolean value with int",
+		})
 	}
 	return nil, nil
 }
--- testdata/src/boolean_comparison/main.go
+++ testdata/src/boolean_comparison/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	goqu.C("bool_column").Eq(true)
+	goqu.C("bool_column").Eq(true) // want `compare boolean value with int`
+
+	goqu.C("bool_column").Eq(0)
+	goqu.C("bool_column").Eq(1)
+
+	var bl bool
+	goqu.C("bool_column").Eq(bl) // want `compare boolean value with int`
+
+	i := 0
+	goqu.C("bool_column").Eq(i)
+
+	goqu.C("bool_column").Eq(goqu.L("TRUE"))
+	goqu.C("bool_column").Eq(goqu.L("FALSE"))
 }
